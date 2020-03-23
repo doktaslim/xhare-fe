95e5fae1624ad38d204369ae590ff96ee765c66e
Chiagoziem Patrick Anyanwu <gozzypat@gmail.com>
2020-03-23 12:47:07 +0100

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,27 +8,6 @@ export default function index() {
       <Link className="nav__nav-logo" to="/">
         xShare
       </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/team">
-              Meet the Team
-            </Link>
-          </li>
-        </ul>
-      </div>
     </nav>
   );
 }
